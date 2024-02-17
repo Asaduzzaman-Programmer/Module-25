b79a36fc26a6c76cc3fba0768c2d5c980d043262
asad <asaduzzamanprogrammer@gmail.com>
2024-02-17 16:09:43 +0600

    moja

--- event.js
+++ event.js
@@ -1,1 +1,10 @@
 console.log("Dom er ajke special Event")
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+const makeGreen = document.getElementById('make-green');
+makeGreen.addEventListener('click', function makegreen (){
+    document.body.style.backgroundColor = 'green'
+})
